63ba4ce16a315857bce8921c99f6e72f0926bf21
btbruno <30850974+btbruno@users.noreply.github.com>
2017-12-10 19:14:15 -0200

    Add files via upload

---
+++ ProjetoSpace/index.php
@@ -0,0 +1,12 @@
+<?php 
+	//========================================
+	// Index
+	//========================================
+	include_once('_cabecalho.php');
+
+
+echo '<h2 class="text-center">Olá mundo</h2>';
+
+	include_once('_rodape.php');
+
+ ?>
